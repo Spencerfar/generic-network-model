0355ee005ba93123f4fca4564281502c5027add3
Spencer <spencerfar@gmail.com>
2019-11-22 23:15:53 -0400

    added documention to rates

--- gnm/rates.py
+++ gnm/rates.py
@@ -1,7 +1,15 @@
 import numpy as np
 
 def damage_rate(f, params):
+    """
+    Default unscaled damage rate (no Gamma0). 
+    Uses gammap = params[0]
+    """
     return np.exp(params[0]*f)
 
 def repair_rate(f, params):
+    """
+    Default unscaled repair rate (no Gamma0). 
+    Uses gamman = params[1], R = params[2]
+    """
     return np.exp(-params[1]*f)/params[2]
