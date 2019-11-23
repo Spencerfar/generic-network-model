00ba51dd14cbf0b3289260e7b6b67565800b4fc2
Spencer <spencerfar@gmail.com>
2019-11-22 23:16:03 -0400

    added documention to tree

--- gnm/fenwick_tree.py
+++ gnm/fenwick_tree.py
@@ -5,11 +5,26 @@ import numpy as np
 class FenwickTree:
     """
     Creates a partial sum tree for quicker evaluation of partial sums.
+    More info here https://en.wikipedia.org/wiki/Fenwick_tree
+
+    Use of this is demonstrated in Gibson, Bruck https://pubs.acs.org/doi/pdf/10.1021/jp993732q
+    In particular figure 7 is a good visualization.
+
+    Parameters
+    ----------
+    
+    n : int
+        Number of leaf nodes at the bottom of tree (rates).
+    
+    initial_rate : int, float
+        Initial value (rate) to set at leaf nodes.
+
     """
     def __init__(self, n, initial_rate):
 
         self.n_total = 0
         self.offset = 0
+        self.n = n
 
         n_round = 1
         while n_round < n:
@@ -25,9 +40,9 @@ class FenwickTree:
         for i in range(self.offset, self.offset + n):
             self.nodes[i] = initial_rate
 
-        self.sumTree()
+        self._sumTree()
 
-    def sumTree(self):
+    def _sumTree(self):
         # sum full tree
 
         for parent in range(self.offset-1, -1, -1):
@@ -38,7 +53,24 @@ class FenwickTree:
         self.total_sum = self.nodes[0]
 
     def update(self, index, value):
+        """
+        Change the value of leaf node index to value, 
+        then update rest of tree.
 
+        Parameters
+        ----------
+        
+        index : int
+            Index of leaf to change.
+
+        
+        value : int, float
+            Value to change to.
+        
+        """
+
+        assert index < self.n, "Error: Trying to access node outside of tree."
+        
         #set value of tree at index to value
         self.nodes[self.offset+index] = value
 
@@ -59,8 +91,24 @@ class FenwickTree:
         self.total_sum = self.nodes[0]
 
     def search(self, value):
-        #search for value
+        """
+        Search tree to determine leaf node i which satisfies, 
+        R[i] = sum(nodes[:i])
+        R[i] < value <= R[i+1].
 
+        Parameters
+        ----------
+
+        value : int, float
+            Value to search for.
+
+        Returns
+        -------
+
+        index : int
+            Index of node found.
+
+        """
         i = 0
         while i < self.offset:
             left_child = 2*i + 1
@@ -73,7 +121,11 @@ class FenwickTree:
         return i - self.offset
 
     def resetTree(self, initial_rate):
-
+        """
+        Reset tree back to initial rate.
+        
+        """
+        
         #reset tree to zero
         for i in range(self.ntotal):
             self.nodes[i] = 0
@@ -82,5 +134,5 @@ class FenwickTree:
         for i in range(self.offset, offset + n):
             self.nodes[i] = initial_rate
 
-        self.sumTree()
+        self._sumTree()
             
