2c5a0fcfb61c89308a68bf73a0b44f67de24ae8c
Spencer <spencerfar@gmail.com>
2019-11-22 23:25:07 -0400

    added node documentation

--- gnm/node.py
+++ gnm/node.py
@@ -2,12 +2,42 @@ import numpy as np
 
 
 class Node:
+    """
+    Node class. Contains all info we need in the nodes for the simulation.
+    
+    Paremeters
+    ----------
 
+    id : int
+        Network id of this node.
+
+    network : list of lists or 2d array, optional, default = []
+        Network to create node from. 
+        If 2d array (adjacency matrix), must have create_neighbours = True.
+        If list of lists (adjacency list), create_neighbours needs to be False, 
+        and neighbours need to be set separately.
+
+    f : float, default = 0
+        Local frailty of node (proportion of damaged neighbours).
+        
+    d : int, default = 0
+        Binary value (0,1) of node.
+        
+    rate : float, default = 0
+        Node damage/repair rate.
+
+    create_neighbours : bool, default = True
+        If True, creates neighbours from adjacency matrix.
+
+    """
     def __init__(self, id, network = [], f = 0, d = 0, rate = 0, create_neighbours = True):
 
         self.id = id
         self.f = f
+
+        assert d == 0 or d == 1, "Error: Node value needs to be binary."
         self.d = d
+        
         self.rate = rate
         self.k = 0
         self.neighbours = []
@@ -19,12 +49,13 @@ class Node:
             
 
     def reset(self):
+        """Reset nodes to rate = 1, d = 0, f = 0"""
         self.rate = 1
         self.d = 0
         self.f = 0
 
     def _setNeighbours(self, network):
-
+        """Set neighbours of network (adjacency list)"""
         n = network.shape[0]
         self.neighbours = np.zeros(self.k,int)
         
